84239185c872eec610042dcee95b627b37972989
bonifaciotorres <facio0.94@gmail.com>
2016-11-20 23:08:24 -0600

    Delete eje3.py

--- eje3.py
+++
@@ -1,6 +0,0 @@
-meses=('enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre')
-d=int(input("ingresa un numero\n"))
-if(d >= 1 and d <= 12):
- print("el mes " + str(d) + " es " + meses[d - 1])
-else:
- print("No es un mes :V")
